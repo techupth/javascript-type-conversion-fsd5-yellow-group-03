84073ef9cc6f3e846aa7eddcb2e5a622de45f54d
พุฒิพงศ์ วราศรีทรัพย์ <kitsune@Kitsus-MacBook.local>
2023-11-23 13:20:00 +0700

    Jing-type-conversion

--- ex-1.js
+++ ex-1.js
@@ -1,6 +1,6 @@
 let accountBalance = 400;
 let depositAmountFromUser = "10";
-
+depositAmountFromUser=Number(10)
 accountBalance = accountBalance + depositAmountFromUser;
 
 let accountBalanceMessage = "Your account balance is " + accountBalance;
